3fc251694c286d352d57f3c20eb0c97a6527caef
lonalee2018 <lonalee2018@afidev.com>
2021-02-01 00:53:08 +0900

    react-navigation-stack setup

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+
+// createStackNavigator 메소드의 두번째 인자는 설정 옵션
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteNamd: 'Index',
+  defaultNavigationOptions: {
+    title: 'BLOGS'
+  }
+})
+
+export default createAppContainer(navigator)
