0df0a3974c9ba72a006d0ed4d64068d7eb4b9e4b
lonalee2018 <lonalee2018@gmail.com>
2021-02-14 13:13:22 +0900

    [mod - refactoring 1]

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,20 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 const BlogContext = React.createContext(); 
 // like a pipe-line between provider and a component
 
+// add a Reducer
+const blogReducer = (state, action) => {
+    console.log('what is state in blogReducer', state)
+    switch (action.type) {
+        case 'Add_Blog_Post':
+            return [...state, { title : `Blog Post #${state.length + 1}`}]
+    
+        default:
+            return state;
+    }
+}
+
 export const BlogProvider = ({ children }) => {
 
-    const [blogPosts, setBlogPosts] = useState([]);
-    // re commit
+    // const [blogPosts, setBlogPosts] = useState([]);
+    const [blogPosts, dispatch] = useReducer(blogReducer, [{ title: 'test'}]);
+
+    // const addBlogPost = () => {
+    //     setBlogPosts([
+    //         ...blogPosts,
+    //         {
+    //             title: `Blog Post #${blogPosts.length + 1}`
+    //         }
+    //     ])
+    // }
 
     const addBlogPost = () => {
-        setBlogPosts([
-            ...blogPosts,
-            {
-                title: `Blog Post #${blogPosts.length + 1}`
-            }
-        ])
+        dispatch({ type: 'Add_Blog_Post'})
     }
 
     return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
